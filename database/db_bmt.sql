1cce595acba891a1757e57185cf511d2e8a556ee
rifki chairil <rfkchrl.03@gmail.com>
2022-03-25 14:20:46 +0700

    database

--- database/db_bmt.sql
+++ database/db_bmt.sql
@@ -23,15 +23,15 @@ DROP TABLE IF EXISTS `admin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `admin` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_admin` int(11) NOT NULL AUTO_INCREMENT,
   `nama` varchar(45) DEFAULT NULL,
   `username` varchar(45) DEFAULT NULL,
   `password` text,
   `alamat` text,
   `jabatan` varchar(45) DEFAULT NULL,
   `level` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id_admin`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +40,7 @@ CREATE TABLE `admin` (
 
 LOCK TABLES `admin` WRITE;
 /*!40000 ALTER TABLE `admin` DISABLE KEYS */;
-INSERT INTO `admin` VALUES (1,'admin','admin','password','jalan','admin','admin'),(2,'karyawan','karyawan','password','jalan','karyawan','kc');
+INSERT INTO `admin` VALUES (1,'admin','admin','password','jalan','admin','admin'),(2,'kepalacabang','kepalacabang','password','jalan','kepalacabang','kc');
 /*!40000 ALTER TABLE `admin` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,14 +52,14 @@ DROP TABLE IF EXISTS `nasabah`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `nasabah` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_nasabah` int(11) NOT NULL AUTO_INCREMENT,
   `nama` varchar(45) DEFAULT NULL,
   `alamat` text,
   `ktp` varchar(45) DEFAULT NULL,
   `telp` varchar(45) DEFAULT NULL,
   `gender` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id_nasabah`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,6 +68,7 @@ CREATE TABLE `nasabah` (
 
 LOCK TABLES `nasabah` WRITE;
 /*!40000 ALTER TABLE `nasabah` DISABLE KEYS */;
+INSERT INTO `nasabah` VALUES (1,'dodits','jalan palems','12333133','2121511','Laki-Laki'),(2,'doddy','jalan singkong','123123','12312','Laki-Laki'),(3,'anggi','jalan','12312312','33233231','Perempuan');
 /*!40000 ALTER TABLE `nasabah` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -79,7 +80,7 @@ DROP TABLE IF EXISTS `penarikan`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `penarikan` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_penarikan` int(11) NOT NULL AUTO_INCREMENT,
   `admin_id` int(11) NOT NULL,
   `rekening_id` int(11) NOT NULL,
   `rekening` varchar(45) DEFAULT NULL,
@@ -87,12 +88,12 @@ CREATE TABLE `penarikan` (
   `tanggal` date DEFAULT NULL,
   `awal` varchar(45) DEFAULT NULL,
   `akhir` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id_penarikan`),
   KEY `fk_transaksi_admin_idx` (`admin_id`),
   KEY `fk_penarikan_rekening1_idx` (`rekening_id`),
-  CONSTRAINT `fk_penarikan_rekening1` FOREIGN KEY (`rekening_id`) REFERENCES `rekening` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_transaksi_admin0` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT `fk_penarikan_rekening1` FOREIGN KEY (`rekening_id`) REFERENCES `rekening` (`id_rekening`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_transaksi_admin0` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id_admin`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -112,17 +113,17 @@ DROP TABLE IF EXISTS `rekening`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `rekening` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_rekening` int(11) NOT NULL AUTO_INCREMENT,
   `nasabah_id` int(11) NOT NULL,
   `nama` varchar(45) DEFAULT NULL,
   `rek` varchar(45) DEFAULT NULL,
   `tgl_buka` date DEFAULT NULL,
   `awal` varchar(45) DEFAULT NULL,
   `akhir` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id_rekening`),
   KEY `fk_rekening_nasabah1_idx` (`nasabah_id`),
-  CONSTRAINT `fk_rekening_nasabah1` FOREIGN KEY (`nasabah_id`) REFERENCES `nasabah` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT `fk_rekening_nasabah1` FOREIGN KEY (`nasabah_id`) REFERENCES `nasabah` (`id_nasabah`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -131,6 +132,7 @@ CREATE TABLE `rekening` (
 
 LOCK TABLES `rekening` WRITE;
 /*!40000 ALTER TABLE `rekening` DISABLE KEYS */;
+INSERT INTO `rekening` VALUES (2,1,'dodits','11111','2022-03-25','200000','250000'),(3,2,'doddy','22222','2022-03-23','100000','117000'),(5,3,'anggi','313131','2022-03-24','25000','25000');
 /*!40000 ALTER TABLE `rekening` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -142,17 +144,20 @@ DROP TABLE IF EXISTS `transaksi`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `transaksi` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_transaksi` int(11) NOT NULL AUTO_INCREMENT,
   `admin_id` int(11) NOT NULL,
+  `rekening_id` int(11) NOT NULL,
   `rekening` varchar(45) DEFAULT NULL,
   `jumlah` varchar(45) DEFAULT NULL,
   `tanggal` date DEFAULT NULL,
   `awal` varchar(45) DEFAULT NULL,
   `akhir` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id_transaksi`),
   KEY `fk_transaksi_admin_idx` (`admin_id`),
-  CONSTRAINT `fk_transaksi_admin` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  KEY `fk_transaksi_rekening1_idx` (`rekening_id`),
+  CONSTRAINT `fk_transaksi_admin` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id_admin`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_transaksi_rekening1` FOREIGN KEY (`rekening_id`) REFERENCES `rekening` (`id_rekening`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -161,7 +166,7 @@ CREATE TABLE `transaksi` (
 
 LOCK TABLES `transaksi` WRITE;
 /*!40000 ALTER TABLE `transaksi` DISABLE KEYS */;
-INSERT INTO `transaksi` VALUES (3,1,'11111','100000','2022-03-22','200000','200000');
+INSERT INTO `transaksi` VALUES (10,1,5,'313131','10000','2022-03-26','135000','25000');
 /*!40000 ALTER TABLE `transaksi` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -174,4 +179,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-22 11:34:28
+-- Dump completed on 2022-03-25 14:20:36
